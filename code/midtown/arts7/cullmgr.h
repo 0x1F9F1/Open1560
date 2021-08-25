c7ccfdcb40baf10c0be4a7e8a2d61b0f7f6d7a7b
Brick <6098371+0x1F9F1@users.noreply.github.com>
2021-08-25 17:11:02 +0100

    Fixed VERSION_STRING

--- code/midtown/arts7/cullmgr.h
+++ code/midtown/arts7/cullmgr.h
@@ -194,4 +194,4 @@ ARTS_IMPORT extern asCullManager* CULLMGR;
 ARTS_IMPORT extern f32 Update3D;
 
 // ?VERSION_STRING@@3PADA
-ARTS_IMPORT extern const char* VERSION_STRING;
+ARTS_EXPORT extern const char* VERSION_STRING;
