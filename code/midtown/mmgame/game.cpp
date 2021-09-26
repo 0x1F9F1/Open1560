b9cabca1bec6692f3d09dd1740193b53bc24fa6f
Brick <6098371+0x1F9F1@users.noreply.github.com>
2021-09-26 15:08:27 +0100

    Fixed CHICAGO check

--- code/midtown/mmgame/game.cpp
+++ code/midtown/mmgame/game.cpp
@@ -694,7 +694,7 @@ b32 mmGame::Init()
         mmCityInfo* city_info = CityList()->GetCurrentCity();
         arts_strcpy(MapName, city_info->MapName);
         arts_strcpy(RaceDir, city_info->RaceDir);
-        CHICAGO = arts_strnicmp(MapName, "chicago", 7);
+        CHICAGO = !arts_strnicmp(MapName, "chicago", 7);
 
         Displayf("%s maps to %s (CHICAGO=%d)", MapName, RaceDir, CHICAGO);
 
