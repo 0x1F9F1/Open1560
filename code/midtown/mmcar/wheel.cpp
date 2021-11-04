b04ea81c91842b292db78c50a6ba46b0e4e1438c
Brick <6098371+0x1F9F1@users.noreply.github.com>
2021-11-04 20:18:41 +0000

    Avoid creating too much wheel smoke at low FPS

--- code/midtown/mmcar/wheel.cpp
+++ code/midtown/mmcar/wheel.cpp
@@ -26,7 +26,7 @@ define_dummy_symbol(mmcar_wheel);
 // TODO: This also needs to factor in the wheel rotation delta
 void mmWheel::GenerateSkidParticles()
 {
-    ParticleCount += ParticleMultiplier * PtxMaxSkidCount * Sim()->GetUpdateDelta();
+    ParticleCount += std::min(ParticleMultiplier * PtxMaxSkidCount * Sim()->GetUpdateDelta(), 1.0f);
 }
 
 static const f32 PtxFrameRate = 60.0f;
