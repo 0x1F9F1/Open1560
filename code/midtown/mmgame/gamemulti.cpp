965b2325afef614f9b47e797014e43d64e44ba89
KcRobin9 <99972691+KcRobin9@users.noreply.github.com>
2023-12-22 18:00:03 +0100

    [1] mmGameMulti::StartXYZ()

--- code/midtown/mmgame/gamemulti.cpp
+++ code/midtown/mmgame/gamemulti.cpp
@@ -21,6 +21,7 @@ define_dummy_symbol(mmgame_gamemulti);
 #include "gamemulti.h"
 
 #include "mmcar/car.h"
+#include "mmcityinfo/state.h"
 #include "mmnetwork/network.h"
 
 void mmGameMulti::NextRace()
@@ -58,4 +59,49 @@ void mmGameMulti::DeactivateMapNetObject(i32 player)
     icon.Position = 0;
     icon.Place = OPP_ICON_BLANK;
     icon.Enabled = false;
+}
+
+void mmGameMulti::StartXYZ(i32 index, Vector3& out_result, Vector3& start_position, f32 rotation, f32 length)
+{
+    Matrix34 transform_matrix;
+    transform_matrix.Identity();
+    transform_matrix.Rotate(YAXIS, rotation);
+    transform_matrix.m3 = start_position;
+
+    Vector3 position_offset;
+
+    if (length >= 7.0f)
+    {
+        switch (index)
+        {
+            case 0: position_offset.Set(2.75, 0.0, 16.0); break;
+            case 1: position_offset.Set(-2.75, 0.0, 16.0); break;
+            case 2: position_offset.Set(5.5, 0.0, 16.0); break;
+            case 3: position_offset.Set(0.0, 0.0, 16.0); break;
+            case 4: position_offset.Set(2.75, 0.0, 34.0); break;
+            case 5: position_offset.Set(-2.75, 0.0, 34.0); break;
+            case 6: position_offset.Set(0.0, 0.0, 34.0); break;
+            case 7: position_offset.Set(5.5, 0.0, 34.0); break;
+            default: position_offset.Set(0.0, 0.0, 0.0); break;
+        }
+        if (MMSTATE.GameMode == mmGameMode::Blitz && !MMSTATE.EventId)
+            position_offset.z *= -1.0;
+    }
+
+    else
+    {
+        switch (index)
+        {
+            case 0: position_offset.Set(2.25, 0.0, 6.0); break;
+            case 1: position_offset.Set(-2.25, 0.0, 6.0); break;
+            case 2: position_offset.Set(4.5, 0.0, 6.0); break;
+            case 4: position_offset.Set(-4.5, 0.0, 0.0); break;
+            case 5: position_offset.Set(4.5, 0.0, -6.0); break;
+            case 6: position_offset.Set(0.0, 0.0, -6.0); break;
+            case 7: position_offset.Set(-4.5, 0.0, -6.0); break;
+            default: position_offset.Set(0.0, 0.0, 0.0); break;
+        }
+    }
+
+    out_result = position_offset ^ transform_matrix;
 }
